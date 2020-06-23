eaac538e26b0e7853a229b30eb4dc3b89891a308
Drew Kirkland <drew.kirkland@gmail.com>
2020-06-23 14:19:39 -0400

    Implemented new method in PostRepository to get all posts by userId. Implemented view for MyPosts.

---
+++ TabloidMVC/Controllers/MyPostsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class MyPostsController : Controller
+    {
+        private readonly PostRepository _postRepo;
+
+        public MyPostsController(IConfiguration config)
+        {
+            _postRepo = new PostRepository(config);
+        }
+
+        // GET: MyPosts
+        public ActionResult Index()
+        {
+            //Get the user ID
+            int userId = GetCurrentUserId();
+
+            List<Post> userPosts = _postRepo.GetAllUserPosts(userId);
+
+            return View(userPosts);
+        }
+
+        // GET: MyPosts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MyPosts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MyPosts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyPosts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MyPosts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyPosts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MyPosts/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+    }
+}
--- TabloidMVC/Repositories/PostRepository.cs
+++ TabloidMVC/Repositories/PostRepository.cs
@@ -33,7 +33,8 @@ namespace TabloidMVC.Repositories
                               LEFT JOIN Category c ON p.CategoryId = c.id
                               LEFT JOIN UserProfile u ON p.UserProfileId = u.id
                               LEFT JOIN UserType ut ON u.UserTypeId = ut.id
-                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()";
+                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()
+                        ORDER BY PublishDateTime DESC";
                     var reader = cmd.ExecuteReader();
 
                     var posts = new List<Post>();
@@ -91,6 +92,8 @@ namespace TabloidMVC.Repositories
             }
         }
 
+        //Gets a single post by POST ID matching specified user (by UserProfileId)
+        //Only returns one post
         public Post GetUserPostById(int id, int userProfileId)
         {
             using (var conn = Connection)
@@ -132,6 +135,48 @@ namespace TabloidMVC.Repositories
             }
         }
 
+        public List<Post> GetAllUserPosts(int userProfileId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT p.Id, p.Title, p.Content, 
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              c.[Name] AS CategoryName,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                              ut.[Name] AS UserTypeName
+                         FROM Post p
+                              LEFT JOIN Category c ON p.CategoryId = c.id
+                              LEFT JOIN UserProfile u ON p.UserProfileId = u.id
+                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                        WHERE p.UserProfileId = @userProfileId
+                        ORDER BY p.CreateDateTime DESC";
+
+                    cmd.Parameters.AddWithValue("@userProfileId", userProfileId);
+                    var reader = cmd.ExecuteReader();
+
+                    List<Post> userPosts = new List<Post>();
+
+                    while (reader.Read())
+                    {
+                        Post post = NewPostFromReader(reader);
+
+                        userPosts.Add(post);
+                    }
+
+                    reader.Close();
+
+                    return userPosts;
+                }
+            }
+        }
 
         public void Add(Post post)
         {
