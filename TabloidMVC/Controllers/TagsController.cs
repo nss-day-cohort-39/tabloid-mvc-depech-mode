6cc37d4dae55c0c0427ce067e7acc2f374fa8d78
Parker Hodge <iparkerhodge@gmail.com>
2020-06-23 15:04:52 -0500

    created the Index and Create views for the tag controller

---
+++ TabloidMVC/Controllers/TagsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class TagsController : Controller
+    {
+        private readonly TagRepository _tagRepo;
+
+        public TagsController(IConfiguration config)
+        {
+            _tagRepo = new TagRepository(config);
+        }
+            // GET: TagsController
+        public ActionResult Index()
+        {
+            List<Tag> tags = _tagRepo.GetAll();
+
+            return View(tags);
+        }
+
+        // GET: TagsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TagsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tag tag)
+        {
+            try
+            {
+                _tagRepo.Add(tag);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(tag);
+            }
+        }
+
+        // GET: TagsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TagsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TagsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TagsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, name FROM Tag
+                                        ORDER BY name ASC";
+                    var reader = cmd.ExecuteReader();
+
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+        public void Add(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Tag (Name)
+                        OUTPUT INSERTED.ID
+                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
