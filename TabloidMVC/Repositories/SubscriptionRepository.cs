98fd6e32e191915321d65a0c5d70b14051c1bd7d
Heidi Sprouse <hsprouse91@gmail.com>
2020-06-25 11:04:49 -0500

    Added SubscriptionRepository

---
+++ TabloidMVC/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class SubscriptionRepository : BaseRepository
+    {
+        public SubscriptionRepository(IConfiguration config) : base(config) { }
+            public List<Tag> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT* FROM Subscription
+                                        WHERE SubscriberUserProfileId = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+    }
+}
