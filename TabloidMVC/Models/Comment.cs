862f9d1d478cd3ef796db2af481797a71159ddf5
Kristen Howton <kristen.howton@gmail.com>
2020-06-25 23:36:51 -0500

    made it where it would not allow a user to add content that was over the limit of the database

--- TabloidMVC/Models/Comment.cs
+++ TabloidMVC/Models/Comment.cs
@@ -16,6 +16,7 @@ namespace TabloidMVC.Models
         public string Subject { get; set; }
 
         [Required(ErrorMessage = "Please enter some content for your comment...")]
+        [StringLength(2^31, MinimumLength = 1)]
         public string Content { get; set; }
 
         [DisplayName("Created")]
